030f0198b5b0dcfecea944945750bbe9778ae8b4
Narinder <getdev84@gmail.com>
2022-03-10 10:23:49 -0500

    Initial setup

---
+++ packages/container/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MarketingApp from './components/marketingApp';
+
+
+const App =()=>{
+
+
+    return(
+    <div>
+
+            <MarketingApp/>
+
+    </div>
+    )
+
+}
+
+export default App;
